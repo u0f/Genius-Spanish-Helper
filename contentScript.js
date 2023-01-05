158b41a02a6fe363485d80b9cb40147ce08c477f
u0f <45316362+u0f@users.noreply.github.com>
2023-01-05 19:20:08 +0100

    Add files via upload

--- contentScript.js
+++ contentScript.js
@@ -7,6 +7,9 @@ const feature = Array.from(document.querySelectorAll(".metadata_unit-label")).fi
 		let header = `[Letra de "${title.innerText}"]
 
 `;
+		btn.classList.add('encabezado-button');
+		document.getElementsByClassName("encabezado-button")[0].style.borderColor = "green"
+		document.getElementsByClassName("encabezado-button")[0].style.color = "green"	
 		btn.addEventListener('click', () => {
 			if (feature) {
 				
@@ -27,14 +30,6 @@ const feature = Array.from(document.querySelectorAll(".metadata_unit-label")).fi
 			ta.value = header + ta.value;
 			changeEvent(ta);
 		});
-	
-		/*const cleanBtn = createSquareButton("Clean");
-		document.querySelector(".lyrics_controls").append(cleanBtn);
-	
-		cleanBtn.addEventListener('click', () => {
-			ta.value = clean(ta.value);
-			changeEvent(ta);
-		});*/
 
 	};
 
@@ -55,80 +50,4 @@ function createSquareButton(text) {
 	b.classList.add("square_button");
 	b.style.marginLeft = "4px";
 	return b;
-}
-
-
-//Made by Original Creator
-function clean(t) {
-	t = t.replace(/<\s([A-Z,a-z])\s>/g, '<$1>');
-	t = t.replace(/<\s([A-Z,a-z])>/g, '<$1>');
-	t = t.replace(/<([A-Z,a-z])\s>/g, '<$1>');
-	t = t.replace(/<\s\/([A-Z,a-z])\s>/g, '</$1>');
-	t = t.replace(/<\s\/([A-Z,a-z])>/g, '</$1>');
-	t = t.replace(/<\/([A-Z,a-z])\s>/g, '</$1>');
-	t = t.replace(/<\s\/\s([A-Z,a-z])\s>/g, '</$1>');
-	t = t.replace(/<\/\s([A-Z,a-z])>/g, '</$1>');
-	t = t.replace(/<\/\s([A-Z,a-z])\s>/g, '</$1>');
-	t = t.replace(/<\s\/\s([A-Z,a-z])\s>/g, '</$1>');
-	t = t.replace(/<[A-Z]/g, x => x.substring(0, 1) + x.charAt(1).toLowerCase() + x.slice(2));
-	t = t.replace(/<\/[A-Z]/g, x => x.substring(0, 2) + x.charAt(2).toLowerCase() + x.slice(3));
-	t = t.replace(/\(\s/g, '(');
-	t = t.replace(/\s\)/g, ')');
-	t = t.replace(/((<[a-z,A-Z]>)+)\s/g, '$1');
-	t = t.replace(/\s((<\/[a-z,A-Z]>)+)/g, '$1'); /*Korrektur d. Anführungszeichen*/
-	t = t.replace(/’/g, '\''); /*Großschreiben des ersten Buchstabens d. Klammer*/
-	t = t.replace(/\(((<[a-z]>)+)\[/g, '[($1');
-	t = t.replace(/(\]\(\d+\))((<\/[a-z]>)+)\)/g, '$2)$1');
-	t = t.replace(/((<[a-z]>)+)\[\(/g, '[($1');
-	t = t.replace(/\)(\]\(\d+\))((<\/[a-z]>)+)/g, '$2)$1'); /*Korrektur der Anordnung von Klammer + Annotation*/
-	t = t.replace(/\(\[/g, '[(');
-	t = t.replace(/(\]\(\d+\))\)/g, ')$1'); /*Korrektur von Klammer + <i>/<b>*/
-	t = t.replace(/((<[a-z]>)+)\(/g, '($1');
-	t = t.replace(/\)((<\/[a-z]>)+)/g, '$1)');
-	var re1 = /(\([a-z])/g;
-	var re2 = /(\(<i>[a-z])/g;
-	var re3 = /(\(<b>[a-z])/g;
-	var re4 = /(\(<i><b>[a-z])/g;
-	var re5 = /(\(<b><i>[a-z])/g;
-	t = t.replace(re1, x => x.substring(0, 1) + x.charAt(1).toUpperCase() + x.slice(2));
-	t = t.replace(re2, x => x.substring(0, 4) + x.charAt(4).toUpperCase() + x.slice(5));
-	t = t.replace(re3, x => x.substring(0, 4) + x.charAt(4).toUpperCase() + x.slice(5));
-	t = t.replace(re4, x => x.substring(0, 7) + x.charAt(7).toUpperCase() + x.slice(8));
-	t = t.replace(re5, x => x.substring(0, 7) + x.charAt(7).toUpperCase() + x.slice(8)); /*Entfernt falsche Spaces vor/nach Satzzeichen*/
-	t = t.replace(/\s:/g, ':');
-	t = t.replace(/:([A-Za-z0-9_äÄöÖüÜßẞ])/g, ': $1');
-	t = t.replace(/\s,/g, ',');
-	t = t.replace(/\s\?/g, '?');
-	t = t.replace(/\s!/g, '!');
-	t = t.replace(/„\s/g, '„');
-	t = t.replace(/\s“/g, '“');
-	t = t.replace(/\.\.\.\.\.\.\./g, '…');
-	t = t.replace(/\.\.\.\.\.\./g, '…');
-	t = t.replace(/\.\.\.\.\./g, '…');
-	t = t.replace(/\.\.\.\./g, '…');
-	t = t.replace(/\.\.\./g, '…');
-	t = t.replace(/\.\./g, '…');
-	t = t.replace(/([A-Za-z0-9_äÄöÖüÜßẞ])…/g, '$1 …');
-	t = t.replace(/\s-\s/g, ' – ');
-	t = t.replace(/\(Parte(.*?):\)/g, '[Parte$1]');
-	t = t.replace(/\[Parte(.*?):\]/g, '[Parte$1]');
-	t = t.replace(/\(Strophe(.*?):\)/g, '[Part$1]');
-	t = t.replace(/\[Strophe(.*?):\]/g, '[Part$1]');
-	t = t.replace(/\(Estribillo(.*?):\)/g, '[Estribillo$1]');
-	t = t.replace(/\[Estribillo(.*?):\]/g, '[Estribillo$1]');
-	t = t.replace(/\(Refrain(.*?):\)/g, '[Estribillo$1]');
-	t = t.replace(/\[Refrain(.*?):\]/g, '[Estribillo$1]');
-	t = t.replace(/\(Intro(.*?):\)/g, '[Intro$1]');
-	t = t.replace(/\[Intro(.*?):\]/g, '[Intro$1]');
-	t = t.replace(/\(Outro(.*?):\)/g, '[Outro$1]');
-	t = t.replace(/\[Outro(.*?):\]/g, '[Outro$1]');
-	t = t.replace(/\(Pre-Estribillo(.*?):\)/g, '[Pre-Estribillo$1]');
-	t = t.replace(/\[Pre-Estribillo(.*?):\]/g, '[Pre-Estribillo$1]');
-	t = t.replace(/\(Pre-Refrain(.*?):\)/g, '[Pre-Estribillo$1]');
-	t = t.replace(/\[Pre-Refrain(.*?):\]/g, '[Pre-Estribillo$1]');
-	t = t.replace(/\(Post-Estribillo(.*?):\)/g, '[Post-Estribillo$1]');
-	t = t.replace(/\[Post-Estribillo(.*?):\]/g, '[Post-Estribillo$1]');
-	t = t.replace(/\(Post-Refrain(.*?):\)/g, '[Post-Estribillo$1]');
-	t = t.replace(/\[Post-Refrain(.*?):\]/g, '[Post-Estribillo$1]');
-	return t;
 }
